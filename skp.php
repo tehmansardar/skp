2dc23ba0432df40f252429f450e38b3b8afd749c
Tehman Sardar <tehmansardar@gmail.com>
2020-07-21 03:04:34 +0500

    Initialize plugin and Added security precautions

---
+++ skp.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * @package skontechplugin
+ */
+
+/* 
+Plugin Name: Skontech
+Plugin URI: https//www.skontech.com/plugin
+Description: Skontech is comprehensive plugin to get all access of wordpress functionalities in quick
+Author: Tehman Sardar
+*/
+
+defined('ABSPATH') or die('Hey you can\t access this, you silly human!');
