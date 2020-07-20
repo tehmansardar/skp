408f8a351ee2759fc6a2f3cafb42e898d05ad3eb
Tehman Sardar <tehmansardar@gmail.com>
2020-07-21 03:08:11 +0500

    OOP practices

--- skp.php
+++ skp.php
@@ -11,3 +11,14 @@ Author: Tehman Sardar
 */
 
 defined('ABSPATH') or die('Hey you can\t access this, you silly human!');
+
+// OOP practices
+class skp{
+    function __construct($string){
+        echo $string;
+    }
+}
+
+if(class_exists('skp')){
+    $skp = new skp('Plugin is activated');
+}
