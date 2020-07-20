cf65b89f7d55107b5b467e46348b8d9ed3a8db9f
Tehman Sardar <tehmansardar@gmail.com>
2020-07-21 03:50:01 +0500

    Uninstall Plugin with data storage in database

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+
+if(!defined('WP_UNINSTALL_PLUGIN')){
+    die;
+}
+//  clear stored database data
+$books = get_post(['post-type'=> 'book', 'numberposts'=> -1]);
+
+foreach($books as $book){
+    wp_delete_post($book->ID, true);
+}
