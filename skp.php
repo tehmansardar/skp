94644ee72869cad0ff6d0bb124e5b34d5220bffa
Tehman Sardar <tehmansardar@gmail.com>
2020-07-21 03:36:40 +0500

    Activate & Deactivation Hook with CPT named BOOK

--- skp.php
+++ skp.php
@@ -12,13 +12,31 @@ Author: Tehman Sardar
 
 defined('ABSPATH') or die('Hey you can\t access this, you silly human!');
 
-// OOP practices
 class skp{
-    function __construct($string){
-        echo $string;
+    function __construct(){
+        add_action('init', [$this, 'custom_post_type']);
+    }
+
+
+    function custom_post_type(){
+        register_post_type('book', ['public'=>true, 'label'=> 'Book']);
+    }
+
+    function activate(){
+        // Generate a CPT
+        $this->custom_post_type();
+        // flush rewrite rules
+        flush_rewrite_rules();
+    }
+    function deactivate(){
+        // flush rewrite rules
+        flush_rewrite_rules();
     }
 }
 
 if(class_exists('skp')){
-    $skp = new skp('Plugin is activated');
+    $skp = new skp();
 }
+
+register_activation_hook(__FILE__, [$skp, 'activate']);
+register_deactivation_hook(__FILE__, [$skp, 'deactivate']);
