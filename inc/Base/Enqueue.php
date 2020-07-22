525b3b4ce7d12fe8117e8d3c0f369b3ca33ebc20
Tehman Sardar <tehmansardar@gmail.com>
2020-07-23 03:32:00 +0500

    define SKP_DEV to clear cache memory

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -7,6 +7,8 @@ namespace Inc\Base;
 
 class Enqueue
 {
+    public $ver;
+
     public function register()
     {
         add_action('admin_enqueue_scripts', [$this, 'enqueue']);
@@ -14,7 +16,8 @@ class Enqueue
 
     public function enqueue()
     {
-        wp_enqueue_style('mypluginstyle', PLUGIN_URL . 'assets/mystyle.css');
-        wp_enqueue_script('mypluginscript', PLUGIN_URL . 'assets/myscript.js');
+        $this->ver = SKP_DEV ? time() : false;
+        wp_enqueue_style('mypluginstyle', PLUGIN_URL . 'assets/mystyle.css', [], $this->ver, 'all');
+        wp_enqueue_script('mypluginscript', PLUGIN_URL . 'assets/myscript.js', [], $this->ver, true);
     }
 }
--- skp.php
+++ skp.php
@@ -18,6 +18,7 @@ if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
 
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('SKP_DEV', true);
 
 // Initialize the core class of plugin
 if (class_exists('Inc\\Init')) {
